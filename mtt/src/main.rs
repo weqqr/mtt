97633654a30ac7e000d9b9a935664019071892ee
weqqr <weqqr@yandex.ru>
2021-09-17 13:52:18 +0300

    Create window

--- mtt/src/main.rs
+++ mtt/src/main.rs
@@ -1,16 +1,58 @@
 #![allow(dead_code)]
 
 mod net;
+mod renderer;
 mod serialize;
 
-use crate::net::Connection;
+use anyhow::Result;
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
 
-fn main() -> anyhow::Result<()> {
-    let mut connection = Connection::new("127.0.0.1:30000")?;
+pub struct App {
+    window: Window,
+}
 
-    connection.send_payload(b"test", true)?;
+impl App {
+    pub fn new(event_loop: &EventLoop<()>) -> Result<Self> {
+        let window = WindowBuilder::new()
+            .with_min_inner_size(PhysicalSize::new(320, 180))
+            .build(&event_loop)?;
 
-    println!("Hello, world!");
+        Ok(Self { window })
+    }
 
-    Ok(())
+    fn handle_event(&mut self, event: WindowEvent) -> Option<ControlFlow> {
+        match event {
+            WindowEvent::CloseRequested => Some(ControlFlow::Exit),
+            _ => None,
+        }
+    }
+
+    fn repaint(&mut self) {
+
+    }
+}
+
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new();
+
+    let mut app = App::new(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        use Event::*;
+
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            WindowEvent { event, .. } => {
+                if let Some(cf) = app.handle_event(event) {
+                    *control_flow = cf;
+                }
+            },
+            RedrawRequested(_) | MainEventsCleared => app.repaint(),
+            _ => (),
+        }
+    });
 }
